88c3027ce00cca350f06741be317a0859aef24a7
Purpureus <purpureusdesign@gmail.com>
2021-06-17 20:40:36 +0200

    JWT test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +13,33 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 app.get('/', (req, res) => {
     res.send('<p>API is working</p>');
+});
+
+app.post('/test', authenticateToken, (req, res) => {
+    res.json(req.user.name);
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = {
+        name: username
+    };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
 });
 
 const PORT = process.env.PORT || 8000;
